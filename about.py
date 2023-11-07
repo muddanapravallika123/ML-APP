e54f7587d7a4cbc092f7fedc794c643f5b98df2d
muddanapravallika123 <122087020+muddanapravallika123@users.noreply.github.com>
2023-11-07 13:22:48 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def app():
+    st.subheader('Pondering is a website created for users to')
+    st.subheader('share their valuable thoughts with the world.')
+    st.markdown('Created by: [Ashwani Siwach](https://github.com/muddanapravallika123)')
+    st.markdown('Contact via mail: [muddanapravallika@gmail.com]')
